d482766fcf64882ed7b4e38f4508d80de5ae0891
liuzhengyang <liuzhengyangpascal@gmail.com>
2014-12-19 22:50:03 +0800

    lzy auto

---
+++ src/main/java/com/kanmeizi/controller/websocket/MyHandler.java
@@ -0,0 +1,21 @@
+package com.kanmeizi.controller.websocket;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+/**
+ * Created by liuzhengyang on 2014/12/19.
+ */
+public class MyHandler extends TextWebSocketHandler {
+
+    private static final Log log = LogFactory.getLog(MyHandler.class);
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        log.info("handle message " + message.getPayload());
+        session.sendMessage(message);
+    }
+}
