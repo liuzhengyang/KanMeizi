de8a933722d37051b2107cfff933addf43a92d24
liuzhengyang <liuzhengyangpascal@gmail.com>
2014-12-21 22:22:14 +0800

    lzy auto

--- src/main/java/com/kanmeizi/controller/websocket/MyHandler.java
+++ src/main/java/com/kanmeizi/controller/websocket/MyHandler.java
@@ -8,6 +8,12 @@ import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import java.io.IOException;
+import java.util.Date;
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * WebSocket 处理类
  * Created by liuzhengyang on 2014/12/19.
@@ -15,11 +21,19 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 public class MyHandler extends TextWebSocketHandler {
 
     private static final Log log = LogFactory.getLog(MyHandler.class);
+    private static final AtomicInteger connectionIds = new AtomicInteger(0);
+    private static final Set<WebSocketSession> connections = new CopyOnWriteArraySet<WebSocketSession>();
+
+    public MyHandler(){
+
+    }
 
     // 连接后
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         log.info(session.getId() + " : " + session.getRemoteAddress() + " has connected");
+        connections.add(session);
+        broadcast(session.getId() + " connected!");
     }
 
     // 处理Pong
@@ -38,6 +52,8 @@ public class MyHandler extends TextWebSocketHandler {
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
         log.info(session.getId() + " : " + session.getRemoteAddress() + " has disconnected");
+        connections.remove(session);
+        broadcast(session.getId() + " disconnected!");
     }
 
     @Override
@@ -49,6 +65,22 @@ public class MyHandler extends TextWebSocketHandler {
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         log.info("handle message " + message.getPayload());
-        session.sendMessage(new TextMessage("you say " + message.getPayload()));
+        broadcast(session.getId() + " : " + message.getPayload());
+    }
+
+    // 广播消息
+    private void broadcast(String msg){
+        for(WebSocketSession client : connections){
+            synchronized (client){
+                try {
+                    client.sendMessage(new TextMessage(msg));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    log.debug("Chat Error: Failed to send message to client", e);
+                    connections.remove(client);
+                    broadcast(client.getId() + " has disconnected");
+                }
+            }
+        }
     }
 }
