a0059a1394cb409cd1a7cb2dc0d34cd0c347cb8d
liuzhengyang <liuzhengyangpascal@gmail.com>
2014-12-21 13:40:44 +0800

    lzy auto

---
+++ src/main/java/com/kanmeizi/util/CountThead.java
@@ -0,0 +1,25 @@
+package com.kanmeizi.util;
+
+import java.util.Calendar;
+
+/**
+ * Created by liuzhengyang on 2014/12/20.
+ */
+public class CountThead implements Runnable {
+    private static boolean state = true;
+    @Override
+    public void run() {
+        while(true){
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Calendar.getInstance());
+        }
+    }
+
+    public static void main(String[] args) {
+        new Thread(new CountThead()).start();
+    }
+}
